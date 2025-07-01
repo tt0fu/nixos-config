4af284115a324f110c0c4fd0a1f4e5d08ca39b3f
ttofu <tt0fu@users.noreply.github.com>
2025-07-01 05:46:20 +0300

    build script

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+FILE_PATH="./core/hardware-configuration.nix"
+
+if [ ! -f "$FILE_PATH" ]; then
+  nixos-generate-config --show-hardware-config > "$FILE_PATH"
+fi
+
 sudo nixos-rebuild switch --flake . --show-trace
