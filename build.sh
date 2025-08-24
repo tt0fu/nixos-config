3827a0b19e827f99561e90706193f5586b5f8df3
ttofu <tt0fu@users.noreply.github.com>
2025-08-24 05:16:38 +0300

    build script fix

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ if [ "$1" == "" ]; then
     command="switch"
 fi
 
-sudo nixos-rebuild $command --flake . --cores 0 --max-jobs 4 --show-trace
+nixos-rebuild $command --flake . --cores 0 --max-jobs 4 --show-trace --sudo
