82b6753cf23a5fb0970cc0ae649f74fccde0aa17
ttofu <tt0fu@users.noreply.github.com>
2025-08-28 00:31:45 +0300

    laptop update

--- flake.nix
+++ flake.nix
@@ -64,6 +64,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
