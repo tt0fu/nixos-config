35c79aa0d1ab2f2b9846943a79c8a8fed244c2b8
ttofu <tt0fu@users.noreply.github.com>
2025-11-12 18:17:12 +0300

    wivrn codec change

--- apps/vr/wivrn.nix
+++ apps/vr/wivrn.nix
@@ -19,7 +19,7 @@
         encoders = [
           {
             encoder = "vaapi";
-            codec = "av1";
+            codec = "h265";
             width = 1.0;
             height = 1.0;
             offset_x = 0.0;
