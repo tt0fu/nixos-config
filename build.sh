a8baddcf1986d4488045e05d9039547b57d38c6d
ttofu <tt0fu@users.noreply.github.com>
2025-11-12 22:21:22 +0300

    switch to proton ge rtsp

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ command=$1;
 if [ "$1" == "" ]; then
     command="switch"
 fi
-
+# --debug --print-build-logs
 nixos-rebuild $command --flake . --cores 0 --max-jobs 4 --show-trace --log-format bar-with-logs --sudo
