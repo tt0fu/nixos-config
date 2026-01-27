36a4b0250af291baeb3820daa6cbb84afac0ae72
ttofu <tt0fu@users.noreply.github.com>
2026-01-27 03:18:13 +0300

    misc changes

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ if [ "$1" == "" ]; then
     command="switch"
 fi
 # --debug --print-build-logs
-nixos-rebuild $command --flake . --cores 0 --max-jobs 4 --show-trace --log-format bar-with-logs --sudo
+nixos-rebuild $command --flake . --cores 0 --show-trace --log-format bar-with-logs --sudo
--- core/networking.nix
+++ core/networking.nix
@@ -1,4 +1,4 @@
-{ pkgs, systemSettings, ... }:
+{ systemSettings, ... }:
 
 {
   networking = {
