b1443e1e9f120cceba73c44af4f3ee23ae0b70a8
ttofu <tt0fu@users.noreply.github.com>
2026-02-12 12:38:01 +0300

    onlyoffice icon

--- modules/progs/office/onlyoffice.nix
+++ modules/progs/office/onlyoffice.nix
@@ -3,5 +3,6 @@
     { pkgs, ... }:
     {
       home.packages = [ pkgs.onlyoffice-desktopeditors ];
+      programs.waybar.settings.mainBar."hyprland/workspaces".window-rewrite."class<ONLYOFFICE>" = "";
     };
 }
