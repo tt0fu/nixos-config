981ed194984133bb5b7d7c668ae404ee17dbdbae
ttofu <tt0fu@users.noreply.github.com>
2026-02-25 18:10:31 +0300

    add entry limit and change bootloader font

--- modules/core/bootloader/default.nix
+++ modules/core/bootloader/default.nix
@@ -5,6 +5,7 @@
     {
       boot.loader = {
         efi.canTouchEfiVariables = true;
+        systemd-boot.configurationLimit = 5;
         grub = {
           enable = true;
           devices = [ "nodev" ];
@@ -13,7 +14,7 @@
           backgroundColor = "#000000";
           theme = pkgs.minimal-grub-theme;
           splashImage = ./grub-bg.png;
-          font = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-Thin.ttf";
+          font = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-Medium.ttf";
         };
       };
     };
