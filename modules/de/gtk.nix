0ffd0085ae4cd4697329f9c9c1125e084301e775
ttofu <tt0fu@users.noreply.github.com>
2026-03-09 20:29:10 +0300

    fix gtk4 apps not theming

--- modules/de/gtk.nix
+++ modules/de/gtk.nix
@@ -7,14 +7,16 @@
   home =
     { pkgs, style, ... }:
     {
-      gtk = {
+      gtk = rec {
         enable = true;
+        colorScheme = "dark";
         theme = {
           package = pkgs.flat-remix-gtk;
           name = "Flat-Remix-GTK-Grey-Darkest";
           # package = pkgs.materia-theme-transparent;
           # name = "Materia-dark";
         };
+        gtk4.theme = theme;
         iconTheme = {
           package = pkgs.adwaita-icon-theme;
           name = "Adwaita";
