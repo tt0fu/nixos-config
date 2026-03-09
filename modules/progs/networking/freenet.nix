610c44817640ce4a834830e70749689b7e093a53
ttofu <tt0fu@users.noreply.github.com>
2026-03-09 20:29:23 +0300

    add freenet service

--- modules/progs/networking/freenet.nix
+++ modules/progs/networking/freenet.nix
@@ -1,18 +1,19 @@
 {
-  # os =
-  #   { pkgs, allModules, ... }:
-  #   {
-  #     systemd.services.freenet = {
-  #       enable = true;
-  #       path = [ (pkgs.callPackage allModules.progs.networking.freenet.package { }) ];
-  #       script = "freenet";
-  #     };
-  #   };
+  os =
+    { ... }:
+    {
+      networking.firewall.allowedUDPPorts = [ 49367 ];
+    };
   home =
     { pkgs, inputs, ... }:
     {
       home.packages = [
         inputs.freenet.packages.${pkgs.stdenv.system}.default
       ];
+      # systemd.user.services.freenet-autoupdate = {
+      #   Service = {
+      #     ExecStart = "${inputs.freenet.packages.${pkgs.stdenv.system}.default}/bin/freenet-autoupdate";
+      #   };
+      # };
     };
 }
