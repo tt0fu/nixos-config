628b64a29b3398e8afe0877c2ce4611e109e5bca
ttofu <tt0fu@users.noreply.github.com>
2026-03-19 14:51:00 +0300

    Fix zen-browser xdg mimeApps

--- modules/progs/essential/zen-browser.nix
+++ modules/progs/essential/zen-browser.nix
@@ -7,6 +7,7 @@
       ];
       programs.zen-browser = {
         enable = true;
+        setAsDefaultBrowser = true;
         policies = {
           DisableAppUpdate = true;
           DisableTelemetry = true;
@@ -15,43 +16,39 @@
         nativeMessagingHosts = [ pkgs.firefoxpwa ];
       };
       home.sessionVariables.MOZ_LEGACY_PROFILES = "1";
-      # xdg.mimeApps =
-        # let
-        #   value =
-        #     let
-        #       zen-browser = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.twilight;
-        #     in
-        #     zen-browser.meta.desktopFileName;
+      xdg.mimeApps =
+        let
+          value = "zen-twilight.desktop";
 
-        #   associations = builtins.listToAttrs (
-        #     map
-        #       (name: {
-        #         inherit name value;
-        #       })
-        #       [
-        #         "application/x-extension-shtml"
-        #         "application/x-extension-xhtml"
-        #         "application/x-extension-html"
-        #         "application/x-extension-xht"
-        #         "application/x-extension-htm"
-        #         "x-scheme-handler/unknown"
-        #         "x-scheme-handler/mailto"
-        #         "x-scheme-handler/chrome"
-        #         "x-scheme-handler/about"
-        #         "x-scheme-handler/https"
-        #         "x-scheme-handler/http"
-        #         "application/xhtml+xml"
-        #         "application/json"
-        #         "application/pdf"
-        #         "text/plain"
-        #         "text/html"
-        #       ]
-        #   );
-        # in
-        # {
-        #   associations.added = associations;
-        #   defaultApplications = associations;
-        # };
+          associations = builtins.listToAttrs (
+            map
+              (name: {
+                inherit name value;
+              })
+              [
+                "application/x-extension-shtml"
+                "application/x-extension-xhtml"
+                "application/x-extension-html"
+                "application/x-extension-xht"
+                "application/x-extension-htm"
+                "x-scheme-handler/unknown"
+                "x-scheme-handler/mailto"
+                "x-scheme-handler/chrome"
+                "x-scheme-handler/about"
+                "x-scheme-handler/https"
+                "x-scheme-handler/http"
+                "application/xhtml+xml"
+                "application/json"
+                "application/pdf"
+                "text/plain"
+                "text/html"
+              ]
+          );
+        in
+        {
+          associations.added = associations;
+          defaultApplications = associations;
+        };
 
       wayland.windowManager.hyprland.settings.bind = [
         "SUPER, Z, exec, zen-twilight"
