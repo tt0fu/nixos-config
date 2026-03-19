aa338ec7f67f6c6061570a77a208a59a715052e4
ttofu <tt0fu@users.noreply.github.com>
2026-03-19 03:40:56 +0300

    change stderred color to gray

--- modules/progs/utils/stderred.nix
+++ modules/progs/utils/stderred.nix
@@ -2,7 +2,10 @@
   home =
     { pkgs, ... }:
     {
-      # programs.bash.bashrcExtra = "export LD_PRELOAD=$LD_PRELOAD:${pkgs.stderred}/lib/libstderred.so";
-      home.sessionVariables.LD_PRELOAD="$LD_PRELOAD:${pkgs.stderred}/lib/libstderred.so";
+      programs.bash.bashrcExtra = ''
+        export STDERRED_ESC_CODE=`echo -e $(tput setaf 8)`
+        export LD_PRELOAD=$LD_PRELOAD:${pkgs.stderred}/lib/libstderred.so
+      '';
+      # home.sessionVariables.LD_PRELOAD="$LD_PRELOAD:${pkgs.stderred}/lib/libstderred.so";
     };
 }
