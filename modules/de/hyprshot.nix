36d95087c61d7e7d2d1d56c4b0e1e7f10fa1b96c
ttofu <tt0fu@users.noreply.github.com>
2026-04-06 00:19:35 +0300

    add full-screen screenshot option

--- modules/de/hyprshot.nix
+++ modules/de/hyprshot.nix
@@ -5,6 +5,7 @@
       home.packages = [ pkgs.hyprshot ];
       wayland.windowManager.hyprland.settings.bind = [
         "SUPER, S, exec, hyprshot -z -o ~/Pictures/Screenshots -m region"
+        "SUPER SHIFT, S, exec, hyprshot -z -o ~/Pictures/Screenshots -m output"
       ];
       # programs.niri.settings.binds."Mod+S".action.spawn = [
       #   "hyprshot"
