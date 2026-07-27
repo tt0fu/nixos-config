b4059fe99f9648520efbfcc98cc76e2dcff7f9f2
ttofu <tt0fu@users.noreply.github.com>
2026-07-27 20:55:43 +0300

    use rclone home-manager option

--- modules/progs/utils/rclone.nix
+++ modules/progs/utils/rclone.nix
@@ -1,15 +1,25 @@
 {
   home =
-    { pkgs, lib, ... }:
+    { userSettings, ... }:
     {
-      home.packages = [ pkgs.rclone ];
-      wayland.windowManager.hyprland.settings.on = [
-        {
-          _args = [
-            "hyprland.start"
-            (lib.generators.mkLuaInline ''function() hl.exec_cmd("rclone mount google-drive:Synced ~/DriveSynced/") end'')
-          ];
-        }
-      ];
+      programs.rclone = {
+        enable = true;
+        remotes.google-drive = {
+          config = {
+            type = "drive";
+            scope = "drive.file";
+            team_drive = "";
+          };
+          mounts."Synced" = {
+            enable = true;
+            mountPoint = "/home/${userSettings.username}/DriveSynced";
+          };
+          secrets = {
+            client_id = "/home/${userSettings.username}/.config/rclone/client_id";
+            client_secret = "/home/${userSettings.username}/.config/rclone/client_secret";
+            token = "/home/${userSettings.username}/.config/rclone/token";
+          };
+        };
+      };
     };
 }
